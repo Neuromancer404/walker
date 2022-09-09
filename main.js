be31925572d5c6020af0bf85d680673df9223fb6
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2022-09-09 16:27:52 +0500

    добавил pointerlockcamera и украл все с misc cpl
    не работает сабака

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-"use strict"
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.144.0/three.module.min.js'
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
+import { PointerLockControls } from "./plc.js"
 
-let scene, camera, renderer, light;
+
+let scene, camera, renderer, light, controls;
 let position = {x: 100, y: 10, z: 0}
-let lookAt = {x: 0, y: 10, z: 0}
+
+const objects = [];
+
+let raycaster;
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const vertex = new THREE.Vector3();
+const color = new THREE.Color();
+
 start3d();
 
 function start3d(){
@@ -13,7 +29,19 @@ function start3d(){
     createLight();
     rendering();
     floorGeometry();
+    createControl();
     animate();
+}
+
+function createControl(){
+    try{
+        raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+        controls = new PointerLockControls(camera, document.body);
+        scene.add( controls.getObject() );
+    }
+    catch(e){
+        console.log(e)
+    }
 }
 
 function floorGeometry(){
@@ -26,16 +54,17 @@ function floorGeometry(){
 }
 
 function createLight(){
-    light = new THREE.DirectionalLight(0xFF0000, 1.0, 0);
+    light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
     light.position.set(100, 100, 200);
     scene.add(light);
 }
 
 function rendering(){
     try{
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild( renderer.domElement );
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		document.body.appendChild( renderer.domElement );
     }
     catch(e){
         console.log(e)
@@ -44,7 +73,7 @@ function rendering(){
 
 function createCamera(){
     try{
-        camera = new THREE.PerspectiveCamera(75, window.innerWidth / innerHeight, 0.1, 1000);
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
         camera.position.set(position.x, position.y, position.z);
 
         camera.lookAt(0, 10, 0);
@@ -58,46 +87,130 @@ function createScene(){
     try{
         scene = new THREE.Scene();
         scene.background = new THREE.Color( 0x00000 );
+        scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
     }catch(e){
         console.log(e)
     }
- }
+}
+function animate() {
 
-function animate(){
     requestAnimationFrame( animate );
+
+    const time = performance.now();
+
+    if ( controls.isLocked === true ) {
+
+        raycaster.ray.origin.copy( controls.getObject().position );
+        raycaster.ray.origin.y -= 10;
+
+        const intersections = raycaster.intersectObjects( objects, false );
+
+        const onObject = intersections.length > 0;
+
+        const delta = ( time - prevTime ) / 1000;
+
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+
+        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+        direction.z = Number( moveForward ) - Number( moveBackward );
+        direction.x = Number( moveRight ) - Number( moveLeft );
+        direction.normalize(); // this ensures consistent movements in all directions
+
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+        if ( onObject === true ) {
+
+            velocity.y = Math.max( 0, velocity.y );
+            canJump = true;
+
+        }
+
+        controls.moveRight( - velocity.x * delta );
+        controls.moveForward( - velocity.z * delta );
+
+        controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+
+        if ( controls.getObject().position.y < 10 ) {
+
+            velocity.y = 0;
+            controls.getObject().position.y = 10;
+
+            canJump = true;
+
+        }
+
+    }
+
+    prevTime = time;
+
     renderer.render( scene, camera );
+
 }
 
-function goFort(){
-    position.x -= 1;
-    camera.position.set(position.x, position.y, position.z);
-}
-function goBack(){
-    position.x += 1;
-    camera.position.set(position.x, position.y, position.z);
-}
-function left(){
-    lookAt.z += 5;
-    camera.lookAt(lookAt.x, lookAt.y, lookAt.z);
-}
-function right(){
-    lookAt.z -= 5;
-    camera.lookAt(lookAt.x, lookAt.y, lookAt.z);
-}
-document.onkeydown = function(event){
-    switch (event.keyCode){
-        case 87:
-            goFort();
+
+const onKeyDown = function ( event ) {
+
+    switch ( event.code ) {
+
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
             break;
-        case 83:
-            goBack();
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = true;
             break;
-        case 68:
-            right();
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
             break;
-        case 65:
-            left();
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = true;
             break;
+
+        case 'Space':
+            if ( canJump === true ) velocity.y += 350;
+            canJump = false;
+            break;
+
     }
-    
-}
+
+};
+
+const onKeyUp = function ( event ) {
+
+    switch ( event.code ) {
+
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = false;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = false;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = false;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = false;
+            break;
+
+    }
+
+};
+
+document.addEventListener( 'keydown', onKeyDown );
+document.addEventListener( 'keyup', onKeyUp );
